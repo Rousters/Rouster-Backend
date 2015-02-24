357cfbdb3b185a01046ff81ab8e5bdb0ce5c1c85
Sean Golob <seangolob@live.com>
2015-02-24 07:28:37 -0800

    add passport auth

--- test/userTest.js
+++ test/userTest.js
@@ -1,5 +1,8 @@
 'use strict';
 
+process.env.MONGO_URI = 'mongodb://localhost/alarm_dev';
+require('../server');
+var mongoose = require('mongoose');
 var chai = require('chai');
 var chaihttp = require('chai-http');
 var expect = chai.expect;
@@ -7,12 +10,29 @@ var expect = chai.expect;
 chai.use(chaihttp);
 
 describe('user route spec', function() {
-  it('has a default index');
-  it('creates a new user');
-  it('authenticates a user');
-  describe('app specific database functionality', function() {
-    it('saves a global daily alarm time');
-    it('saves daily wake time');
+
+  before(function(done) {
+    chai.request('localhost:3000')
+      .post('/create_user')
+      .send({id: 'testID'})
+      .end(function(err, res){
+        expect(err).to.eql(null);
+        done();
+      });
   });
+
+  it('has a default index');
+  // it('creates a new user');
+  // it('authenticates a user');
+  // describe('app specific database functionality', function() {
+  //   it('saves a global daily alarm time');
+  //   it('saves daily wake time');
+  // });
+
+  // after(function(done){
+  //   mongoose.connection.db.dropDatabase(function(){
+  //     done();
+  //   });
+  // });
 
 });
