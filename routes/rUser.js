59b1fe946c67bb95ba4e0b102ce57c01abec1aec
Ian McCunn <ianmccunn@gmail.com>
2015-02-24 12:05:11 -0800

    tests for /create_user, /user/id, and alarm data, .jscsrc config added

--- routes/rUser.js
+++ routes/rUser.js
@@ -4,14 +4,21 @@ var bodyparser = require('body-parser');
 var User = require('../models/User.js');
 
 module.exports = function(app) {
+
   app.use(bodyparser.json());
+
   app.post('/create_user', function(req, res) {
-    console.log('hit user');
+    console.log('post request for /create_user');
+
     var newUser = new User();
     newUser.basic.id = newUser.hashID(req.body.id);
     newUser.save(function(err, user){
-      if (err) return res.status(500).send({msg: 'could not create user'});
+      if (err) {
+        res.status(500).send({msg: 'could not create user'});
+        return;
+      }
       res.json({msg: 'user created!'});
     });
   });
+  
 }
