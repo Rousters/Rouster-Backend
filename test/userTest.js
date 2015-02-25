e86eaab922b00f201474d9e2c1367cd52d4d9b75
Ian McCunn <ianmccunn@gmail.com>
2015-02-25 15:42:40 -0800

    cleaning test file and removing more logs

--- test/userTest.js
+++ test/userTest.js
@@ -11,58 +11,54 @@ chai.use(chaihttp);
 
 var server = 'localhost:3000';
 
-describe('/create_user, /user, and /user/id/alarm routes spec', function() {
+describe('/check_alarm specific route', function() {
+  var token;
   var id = 'testID';
-
-  describe('/check_alarm specific route', function() {
-    var token;
-    before(function(done) { // jshint ignore: line
-      chai.request(server)
-        .post('/create_user')
-        .send({id: id})
-        .end(function(err, res) {
-          expect(err).to.eql(null);
-          expect(res).to.have.status(200);
-          expect(res.body).to.have.property('eat');
-          token = res.body.eat;
-          done();
-        });
-    });
-    it('post should create alarm information for a user', function(done) {
-      chai.request(server)
-        .post('/create_alarm')
-        .send({time: 100, id: id, eat: token})
-        .end(function(err, res) {
-          expect(err).to.eql(null);
-          expect(res).to.have.status(200);
-          expect(res.body.msg).to.eql('alarm created');
-          done();
-        });
-    });
-    it('patch should add point if wake time met', function(done) {
-      chai.request(server)
-        .patch('/check_alarm')
-        .send({id: id, eat: token, wakeTime: 104})
-        .end(function(err, res) {
-          expect(err).to.eql(null);
-          expect(res).to.have.status(200);
-          expect(res.body.msg).to.eql('point added!');
-          done();
-        });
-    });
-    it('patch should not add point if wake time not met', function(done) {
-      chai.request(server)
-        .patch('/check_alarm')
-        .send({id: id, eat: token, wakeTime: 110})
-        .end(function(err, res) {
-          expect(err).to.eql(null);
-          expect(res).to.have.status(200);
-          expect(res.body.msg).to.eql('you suck');
-          done();
-        });
-    });
+  before(function(done) { // jshint ignore: line
+    chai.request(server)
+      .post('/create_user')
+      .send({id: id})
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('eat');
+        token = res.body.eat;
+        done();
+      });
   });
-
+  it('post should create alarm information for a user', function(done) {
+    chai.request(server)
+      .post('/create_alarm')
+      .send({time: 100, id: id, eat: token})
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.body.msg).to.eql('alarm created');
+        done();
+      });
+  });
+  it('patch should add point if wake time met', function(done) {
+    chai.request(server)
+      .patch('/check_alarm')
+      .send({id: id, eat: token, wakeTime: 104})
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.body.msg).to.eql('point added!');
+        done();
+      });
+  });
+  it('patch should not add point if wake time not met', function(done) {
+    chai.request(server)
+      .patch('/check_alarm')
+      .send({id: id, eat: token, wakeTime: 110})
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.body.msg).to.eql('you suck');
+        done();
+      });
+  });
   after(function(done) {
     mongoose.connection.db.dropDatabase(function() {
       done();
