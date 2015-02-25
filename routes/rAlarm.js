383ff3d2d5a287711faf7487cb27210dd8862dfe
Ian McCunn <ianmccunn@gmail.com>
2015-02-25 14:56:35 -0800

    cleaning code for route rUser

--- routes/rAlarm.js
+++ routes/rAlarm.js
@@ -8,29 +8,37 @@ var eat_auth = require('../lib/eat_auth');
 module.exports = function(app, appSecret){
   app.use(bodyparser.json());
   app.post('/create_alarm', eat_auth(appSecret), function(req, res){
-    console.log('hit create alarm!');
     var newAlarm = new Alarm();
     newAlarm.alarmTime = req.body.time;
     newAlarm.id = req.body.id;
     newAlarm.save(function(err, alarm){
-      if (err) return res.status(500).send({msg: 'could not create alarm'});
+      if (err) {
+        res.status(500).send({msg: 'could not create alarm'});
+        return;
+      }
       res.json({msg: 'alarm created'});
     });
   });
 
   app.patch('/check_alarm', eat_auth(appSecret), function(req, res){
-    console.log('hit check alarm!');
     Alarm.findOneAndUpdate({id: req.body.id}, {wakeTime: req.body.wakeTime}, function(err, alarm){
-      if (err) return res.status(500).send({msg: 'could not check alarm'});
+      if (err) {
+        res.status(500).send({msg: 'could not check alarm'});
+        return;
+      }
       if(alarm.compareTimes()){
         console.log('got past comparetimes');
         User.findOneAndUpdate({id: req.body.id}, {$inc: {pointCount: 1}}, function(err, user){
-          if (err) return res.status(500).send({msg: 'could not add to pointcount'});
+          if (err) {
+            res.status(500).send({msg: 'could not add to pointcount'});
+          }
           res.json({msg: 'point added!'});
 
         });
       }
-      else {res.json({msg: 'you suck'})};
+      else {
+        res.json({msg: 'you suck'})
+      }
     });
 
   });
